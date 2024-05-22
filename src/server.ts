b670b1711e00e068a5900fad747083d216e1fedb
Muqeet Ahmad <inoxentuni1@gmaill.com>
2024-05-22 16:49:29 +0500

    netlify

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,37 @@
+// import express, { Application } from "express";
+// import mongoose from "mongoose";
+// import path from "path";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import userRoutes from "./routes/userRoutes";
+// import ServerlessHttp = require("serverless-http");
+
+// dotenv.config();
+// const app: Application = express();
+// app.use(cors());
+// const PORT: number = parseInt(process.env.PORT || "8000");
+
+// mongoose
+//   .connect(process.env.MONGO_URL as string)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
+// app.use(bodyParser.json());
+// app.use("/api", userRoutes);
+
+// app.use(express.static(path.join(__dirname, "../../web/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../web/build", "index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+ // "build": "npx tsc"
+
+
+
 import express, { Application } from "express";
 import mongoose from "mongoose";
 import path from "path";
@@ -5,6 +39,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes";
+import ServerlessHttp = require("serverless-http");
 
 dotenv.config();
 const app: Application = express();
@@ -24,6 +59,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../web/build", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports.handler = ServerlessHttp(app);
