9f9948727e2ebb944ce0cff1e95da9ab72e97a78
Muqeet Ahmad <inoxentuni1@gmaill.com>
2024-05-22 19:24:05 +0500

    netlify

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,8 +1,34 @@
 "use strict";
+// import express, { Application } from "express";
+// import mongoose from "mongoose";
+// import path from "path";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import userRoutes from "./routes/userRoutes";
+// import ServerlessHttp = require("serverless-http");
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// dotenv.config();
+// const app: Application = express();
+// app.use(cors());
+// const PORT: number = parseInt(process.env.PORT || "8000");
+// mongoose
+//   .connect(process.env.MONGO_URL as string)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+// app.use(bodyParser.json());
+// app.use("/api", userRoutes);
+// app.use(express.static(path.join(__dirname, "../../web/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../web/build", "index.html"));
+// });
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+// "build": "npx tsc"
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const path_1 = __importDefault(require("path"));
@@ -10,6 +36,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const ServerlessHttp = require("serverless-http");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
@@ -26,4 +53,5 @@ app.get("*", (req, res) => {
 });
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+  });
+module.exports.handler = ServerlessHttp(app);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,36 @@
+// import express, { Application } from "express";
+// import mongoose from "mongoose";
+// import path from "path";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import userRoutes from "./routes/userRoutes";
+// import ServerlessHttp = require("serverless-http");
+
+// dotenv.config();
+// const app: Application = express();
+// app.use(cors());
+// const PORT: number = parseInt(process.env.PORT || "8000");
+
+// mongoose
+//   .connect(process.env.MONGO_URL as string)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
+// app.use(bodyParser.json());
+// app.use("/api", userRoutes);
+
+// app.use(express.static(path.join(__dirname, "../../web/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../web/build", "index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
 import express, { Application } from "express";
 import mongoose from "mongoose";
 import path from "path";
@@ -5,11 +38,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes";
+import serverless from "serverless-http";
 
 dotenv.config();
 const app: Application = express();
 app.use(cors());
 const PORT: number = parseInt(process.env.PORT || "8000");
+
+console.log("port", PORT);
 
 mongoose
   .connect(process.env.MONGO_URL as string)
@@ -19,11 +55,12 @@ mongoose
 app.use(bodyParser.json());
 app.use("/api", userRoutes);
 
-app.use(express.static(path.join(__dirname, "../../web/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../web/build", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+// Export the app wrapped with serverless-http
+export const handler = serverless(app);
+
+// Start the server
